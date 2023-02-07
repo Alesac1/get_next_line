227c2330a7b6f8e4c8d5396e28ca7cf495f88b9c
Alessio Sacchini <asacchin@e3r7p5.42roma.it>
2023-02-07 12:18:09 +0100

    add ex

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,19 @@
 /*   By: asacchin <alesacchi1907@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 19:58:28 by asacchin          #+#    #+#             */
-/*   Updated: 2023/02/05 19:58:30 by asacchin         ###   ########.fr       */
+/*   Updated: 2023/02/07 11:56:17 by asacchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
 # include <unistd.h>
+# include <stdlib.h>
 
+char	*get_next_line(int fd);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,32 +6,102 @@
 /*   By: asacchin <alesacchi1907@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 20:00:34 by asacchin          #+#    #+#             */
-/*   Updated: 2023/02/06 17:52:18 by asacchin         ###   ########.fr       */
+/*   Updated: 2023/02/07 12:12:20 by asacchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* Questo ciclo "while" continua a eseguirsi finché o La funzione 
-	"ft_strchr" restituisce un valore diverso da NULL, indicando che il 
-	carattere di nuova riga ('\n') è stato trovato nella stringa "line" 
-	o Il valore restituito da "read" è minore o uguale a zero, indicando 
-	che non ci sono più dati da leggere dal file descrittore 
-	o che si è verificato un errore. */
+char	ft_strchr(const	char *s, int c)
+{
+	while (*s != '\0' && c != s)
+		s++;
+	if (*s == c)
+		return ((char *)s);
+	return (0);
 }
+
+char	ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
 char	*get_line(int fd, char *line)
 {
 	char	*buf;
-	size_t	b;
+	size_t	i;
 
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	b = 1;
-	while (!(ft_strchr(line, '\n')) && b > 0)
+	i = 1;
+	while (!(ft_strchr(line, '\n')) && i > 0)
 	{
-		b = read( fd, *buf, BUFFER_SIZE);
-		if (b == -1)
+		i = read(fd, *buf, BUFFER_SIZE);
+		if (i == -1)
 		{
 			free(buf);
 			return (NULL);
@@ -55,5 +125,8 @@ char	*clean_new_line(char *line)
 
 get_next_line(int fd)
 {
-	
+	static char *line;
+	char 		*string;
+
+	return (string);
 }
